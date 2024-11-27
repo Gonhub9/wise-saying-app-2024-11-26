c201418a85df0fd5573c278667d6a2db3a07725f
Gonhub9 <ho_gok@naver.com>
2024-11-27 17:35:10 +0900

    study

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,17 +1,40 @@
 package com.ll;
 
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        lab1();
+        // lab1();
+        // lab2();
+
     }
-        private static void lab1() {
-            Scanner scanner = new Scanner(System.in);
 
-            System.out.print("명령 ) ");
-            String cmd = scanner.nextLine().trim();
+    private static void lab1() {
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("입력한 명령 : " + cmd);
+        System.out.print("명령 ) ");
+        String cmd = scanner.nextLine().trim();
 
-        }
+        System.out.println("입력한 명령 : " + cmd);
+
+    }
+
+    private static void lab2() {
+        InputStream in = new ByteArrayInputStream("안녕\n잘가".getBytes());
+
+        Scanner scanner = new Scanner(in);
+
+        System.out.println("명령 ) ");
+        String cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령 : " + cmd);
+
+        System.out.println("명령 ) ");
+        cmd = scanner.nextLine().trim();
+
+        System.out.println("입력한 명령 : " + cmd);
+
+    }
+}
