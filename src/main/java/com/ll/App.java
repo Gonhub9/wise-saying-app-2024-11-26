8785ed55e44123642ced8eb7db543e2a7af9111d
Gonhub9 <ho_gok@naver.com>
2024-11-28 15:06:31 +0900

    study

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,13 +1,17 @@
 package com.ll;
 
+import com.ll.domain.system.controller.SystemController;
+
 import java.util.Scanner;
 
 public class App {
 
     private final Scanner scanner;
+    private final SystemController systemController;
 
     public App(Scanner scanner) {
         this.scanner = scanner;
+        this.systemController = new SystemController();
     }
 
     public void run() {
@@ -18,7 +22,10 @@ public class App {
             System.out.print("명령) ");
             String cmd = scanner.nextLine();
 
-            if ( cmd.equals("종료") ) break;
+            if ( "종료".equals(cmd) ) {
+                systemController.actionExit();
+                break;
+            }
 
 
         }
---
+++ src/main/java/com/ll/domain/system/controller/SystemController.java
@@ -0,0 +1,7 @@
+package com.ll.domain.system.controller;
+
+public class SystemController {
+    public void actionExit() {
+        System.out.println("명언 앱이 종료되었습니다.");
+    }
+}
