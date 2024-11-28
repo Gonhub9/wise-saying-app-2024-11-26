585340a31c50f331ac1b1f91571493795295a2e2
Gonhub9 <ho_gok@naver.com>
2024-11-28 09:24:12 +0900

    study

--- src/main/java/com/ll/standard/util/TestUtil.java
+++ src/main/java/com/ll/standard/util/TestUtil.java
@@ -1,10 +1,14 @@
 package com.ll.standard.util;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 public class TestUtil {
     public static Scanner getScanner(String input) {
         return new Scanner(input);
 
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream byteArrayOutputStream) {
     }
 }
