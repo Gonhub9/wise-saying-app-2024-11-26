d59ed244b405f326518d05c4b6f064b98acd117c
Gonhub9 <ho_gok@naver.com>
2024-11-28 09:39:05 +0900

    study

--- src/main/java/com/ll/standard/util/TestUtil.java
+++ src/main/java/com/ll/standard/util/TestUtil.java
@@ -1,6 +1,6 @@
 package com.ll.standard.util;
 
-import java.io.ByteArrayOutputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
@@ -9,6 +9,19 @@ public class TestUtil {
 
     }
 
+    public static ByteArrayOutputStream setOutToByteArray() {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+
+        return output;
+    }
+
     public static void clearSetOutToByteArray(ByteArrayOutputStream byteArrayOutputStream) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+            byteArrayOutputStream.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/ll/standard/util/TestUtilTest.java
+++ src/main/java/com/ll/standard/util/TestUtilTest.java
@@ -1,9 +1,14 @@
 package com.ll.standard.util;
 
-import java.io.ByteArrayOutputStream;
-import java.util.Scanner;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 
 public class TestUtilTest {
     @Test
@@ -21,15 +26,15 @@ public class TestUtilTest {
         String author = scanner.nextLine();
 
         assertThat(cmd).isEqualTo("등록");
-        assertThat(cmd).isEqualTo("등록");
-        assertThat(cmd).isEqualTo("등록");
+        assertThat(content).isEqualTo("등록");
+        assertThat(author).isEqualTo("등록");
 
     }
 
     @Test
     @DisplayName("TestUtil.setOutToByteArray()")
 
-    public void t1() {
+    public void t2() {
         ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
 
         System.out.println("2 / 이순신 / 나의 죽음을 적들에게 알리지 말라!");
